323c9efaca74ad1e41f96e11ad363cd190b7e942
viylouu <honeefunnee@gmail.com>
2025-01-11 23:22:21 -0700

    combined init.cs and main.cs in src

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,21 @@
 ﻿using SimulationFramework;
+using SimulationFramework.Drawing;
+
+using thrustr.basic;
 
 partial class main {
     static void Main() {
         Simulation sim = Simulation.Create(init, rend);
         sim.Run();
+    }
+    
+    static void init() {
+        Simulation.SetFixedResolution(640, 360, Color.Black);
+
+        intro.initintro();
+
+        Window.SetIcon(Graphics.LoadTexture(@"thrustr\assets\sprites\icons\engine small.png"));
+
+        Window.Title = "thrustr engine";
     }
 }
